7ac82a65ae6ea76e1c9fc01b20ae12b37ed7c47b
Heva <he.wu@linecorp.com>
2018-12-03 18:41:44 +0900

    Create 2 view controllers and push them one by one after clicking next button in each page

--- PushNotifications/PushNotifications/ViewController.swift
+++ PushNotifications/PushNotifications/ViewController.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+}
+
+class SecondViewController: UIViewController {
+
+}
+
+class ThirdViewController: UIViewController {
 
 }
 

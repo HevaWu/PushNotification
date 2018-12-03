be962f8123e3c47c59b14cac1317683133218e28
Heva <he.wu@linecorp.com>
2018-12-03 16:31:05 +0900

    Generate a Xcode project

---
+++ PushNotifications/PushNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PushNotifications
+//
+//  Created by ST21235 on 2018/12/03.
+//  Copyright © 2018 Heva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PushNotifications/PushNotificationsTests/PushNotificationsTests.swift
@@ -0,0 +1,34 @@
+//
+//  PushNotificationsTests.swift
+//  PushNotificationsTests
+//
+//  Created by ST21235 on 2018/12/03.
+//  Copyright © 2018 Heva. All rights reserved.
+//
+
+import XCTest
+@testable import PushNotifications
+
+class PushNotificationsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
